0f30be5e6b50d9c8ad40f368e42de87322fa5e8a
Isala Piyarisi <code@isala.me>
2019-06-20 17:24:01 +0530

    Changed If else to Switch Case

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("\t3) Display Scoreboard");
         System.out.println("\t4) Exit Program");
 
-        for(;;) {
+        while(true) {
             System.out.print("> ");
             // Prevent User from entering a Letter
             if(!stdin.hasNextInt()){
@@ -23,71 +23,75 @@ public class Main {
                 continue;
             }
             int option = stdin.nextInt();
-            if(option == 1){
-                System.out.print("How many golfers in the group: ");
-                int n_golfers;
-                if(stdin.hasNextInt()) {
-                    n_golfers = stdin.nextInt();
-                }
-                else{
-                    invalidInputError();
-                    stdin = new Scanner(System.in);
-                    continue;
-                }
-                for (int i = 0; i < n_golfers; i++) {
+
+
+            switch (option){
+                case 1:
+                    System.out.print("How many golfers in the group: ");
+                    int n_golfers;
+                    if(stdin.hasNextInt()) {
+                        n_golfers = stdin.nextInt();
+                    }
+                    else{
+                        invalidInputError();
+                        stdin = new Scanner(System.in);
+                        break;
+                    }
+                    for (int i = 0; i < n_golfers; i++) {
+                        System.out.print("Name: ");
+                        String name = stdin.next();
+                        // If the name is not in the HashMap this getter will return null
+                        if (getScores(name) != null) {
+                            System.out.println("\n" + name + " already exist, \n\tEnter -1 for score if you don't want to override the current value");
+                        }
+                        System.out.print("Result: ");
+                        // Prevent User from entering a Letter
+                        if (stdin.hasNextInt()) {
+                            int score = stdin.nextInt();
+                            if (score >= 18 && score <= 108) {
+                                setScores(name, score);
+                                continue;
+                            } else if (score == -1) {
+                                continue;
+                            }
+                            // This line only comes if the score is not in range
+                            System.out.println("\nERROR 400: Invalid Result,\n\tMake sure your result is between 18 and 108\n");
+                        } else {
+                            stdin = new Scanner(System.in);
+                        }
+                    }
+                    break;
+
+                case 2:
                     System.out.print("Name: ");
                     String name = stdin.next();
-                    // If the name is not in the HashMap this getter will return null
                     if (getScores(name) != null) {
-                        System.out.println("\n" + name + " already exist, \n\tEnter -1 for score if you don't want to override the current value");
+                        System.out.println(name + "\t - " + getScores(name));
                     }
-                    System.out.print("Result: ");
-                    // Prevent User from entering a Letter
-                    if (stdin.hasNextInt()) {
-                        int score = stdin.nextInt();
-                        if (score >= 18 && score <= 108) {
-                            setScores(name, score);
-                            continue;
-                        } else if (score == -1) {
-                            continue;
-                        }
-                        // This line only comes if the score is not in range
-                        System.out.println("\nERROR 400: Invalid Result,\n\tMake sure your result is between 18 and 108\n");
-                    } else {
-                        stdin = new Scanner(System.in);
+                    else {
+                        System.out.println("ERROR 404: Player not found");
                     }
-                }
+                    break;
 
-            }
-            else if(option == 2){
-                System.out.print("Name: ");
-                String name = stdin.next();
-                if (getScores(name) != null) {
-                    System.out.println(name + "\t - " + getScores(name));
-                }
-                else {
-                    System.out.println("ERROR 404: Player not found");
-                }
+                case 3:
+                    // Sorting Method Taken from http://lewandowski.io/2014/04/java8-map/
+                    for (Map.Entry<String,Integer> score : scores.entrySet()
+                            .stream()
+                            .sorted(Map.Entry.comparingByValue())
+                            .collect(Collectors.toList())) {
+                        System.out.println(score.getKey() + "\t - " + score.getValue());
+                    }
+                    break;
 
-            }
-            else if(option == 3){
-                // Sorting Method Taken from http://lewandowski.io/2014/04/java8-map/
-                for (Map.Entry<String,Integer> score : scores.entrySet()
-                        .stream()
-                        .sorted(Map.Entry.comparingByValue())
-                        .collect(Collectors.toList())) {
-                    System.out.println(score.getKey() + "\t - " + score.getValue());
-                }
+                case 4:
+                    // Exit the Infinite Loop
+                    return;
 
-            }
-            else if(option == 4){
-                // Exit the Infinite Loop
-                break;
-            }
-            else{
-                invalidInputError();
-                // Reset the Standard In Reader
-                stdin = new Scanner(System.in);
+                default:
+                    invalidInputError();
+                    // Reset the Standard In Reader
+                    stdin = new Scanner(System.in);
+                    break;
             }
         }
     }
