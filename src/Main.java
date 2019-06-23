911c6b5f5cc80879675e40b798b9e674e7d92318
Isala Piyarisi <code@isala.me>
2019-06-23 13:09:10 +0530

    Removed While Loops

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     private static Map<String, Integer> scores = new HashMap<>();
+    private static boolean repeatEntry = false;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,47 +14,61 @@ public class Main {
         System.out.println("\t3) Display Scoreboard");
         System.out.println("\t4) Exit Program");
 
-        while(true) {
-            System.out.print("$ ");
-            // Prevent User from entering a Letter
-            if(!sc.hasNextInt()){
-                invalidInputError(sc);
-                continue;
-            }
-            int option = sc.nextInt();
+        // Prevent User from entering a Letter
+        System.out.print("$ ");
+        while (!sc.hasNextInt()){
+            invalidInputError(sc);
             sc.nextLine();
+        }
+        int option = sc.nextInt();
 
+
+        while(option != 4) {
             switch (option){
                 case 1:
                     System.out.print("How many golfers in the group: ");
+
                     int n_golfers;
+
                     if(sc.hasNextInt()) {
                         n_golfers = sc.nextInt();
                     }
                     else{
                         invalidInputError(sc);
-                        break;
+                        continue;
                     }
+
                     for (int i = 0; i < n_golfers; i++) {
+                        System.out.println("\nGOLFER "+ (i+1));
+
                         sc.nextLine();
                         System.out.print("Name: ");
                         String name = sc.nextLine();
+
                         // If the name is not in the HashMap this getter will return null
                         if (getScores(name) != null) {
                             System.out.println("\n" + name + " already exist, \n\tEnter -1 for score if you don't want to override the current value");
+                            repeatEntry = true;
                         }
+
                         System.out.print("Result: ");
+
                         // Prevent User from entering a Letter
                         if (sc.hasNextInt()) {
                             int score = sc.nextInt();
+
                             if (score >= 18 && score <= 108) {
                                 setScores(name, score);
                                 continue;
-                            } else if (score == -1) {
+                            } else if (score == -1 && repeatEntry) {
+                                repeatEntry = false;
+                                i--;
                                 continue;
                             }
+
                             // This line only comes if the score is not in range
-                            System.out.println("\nERROR 400: Invalid Result,\n\tMake sure your result is between 18 and 108\n");
+                            System.out.println("\nERROR 400: Invalid Result,\n\tMake sure your result is between 18 and 108, Enter Again !\n");
+                            i--;
                         } else {
                             invalidInputError(sc);
                         }
@@ -61,17 +76,21 @@ public class Main {
                     break;
 
                 case 2:
+
                     System.out.print("Name: ");
                     String name = sc.nextLine();
+
                     if (getScores(name) != null) {
                         System.out.println(name + "\t - " + getScores(name));
                     }
                     else {
                         System.out.println("ERROR 404: Player not found");
                     }
+
                     break;
 
                 case 3:
+
                     // Sorting Method Taken from http://lewandowski.io/2014/04/java8-map/
                     for (Map.Entry<String,Integer> score : scores.entrySet()
                             .stream()
@@ -79,16 +98,25 @@ public class Main {
                             .collect(Collectors.toList())) {
                         System.out.println(score.getKey() + "\t - " + score.getValue());
                     }
+
                     break;
 
-                case 4:
-                    // Exit the Infinite Loop
-                    return;
 
                 default:
+
                     invalidInputError(sc);
-                    break;
+                    continue;
             }
+
+
+            System.out.print("$ ");
+
+            if(!sc.hasNextInt()){
+                invalidInputError(sc);
+                continue;
+            }
+            option = sc.nextInt();
+            sc.nextLine();
         }
     }
 
@@ -99,7 +127,7 @@ public class Main {
         System.out.println("\t1) Enter Scores");
         System.out.println("\t2) Find Golfer");
         System.out.println("\t3) Display Scoreboard");
-        System.out.println("\t4) Exit Program");
+        System.out.println("\t4) Exit Program \n");
         // Reset the Standard In Reader
         sc.nextLine();
 
